86b5efffc5f7cd7a0fdbf8c1218bd98029139025
Tala Dowiekat <122045458+taladowiekat@users.noreply.github.com>
2024-10-06 21:42:58 +0300

    Time: 31 ms (56.90%) | Memory: 11.4 MB (96.47%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        num={
+            'I' :1,
+            'V' :5,
+            'X' :10,
+            'L' :50,
+            'C' :100,
+            'D' :500,
+            'M' :1000
+        }
+        result = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and num[s[i]] < num[s[i+1]]:
+                result -= num[s[i]]
+            else:
+                result += num[s[i]]
+        return result
+
+
